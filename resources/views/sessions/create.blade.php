ee2e4209e6d0c8c2e3a53531605a93f9d552f8cd
Jeremy Danner <jeremydanner2@gmail.com>
2017-07-20 20:49:14 +0200

    added route for storing a user in the DB

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -3,7 +3,30 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <h1>Register</h1>
+            <h2>Registrera en ny användare</h2>
+
+            <form class="" action="index.html" method="post">
+                {{ csrf_field() }}
+
+                <div class="form-group">
+                    <label for="name">Namn:</label>
+                    <input class="form-control" type="text" name="name" placeholder="Anders Andersson">
+                </div>
+
+                <div class="form-group">
+                    <label for="email">E-post:</label>
+                    <input class="form-control" type="email" name="email" placeholder="Anders.Andersson@schiedel.se">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Lösenord:</label>
+                    <input class="form-control" type="password" name="password">
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">Registrera ny användare</button>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 /*
-* create a new session
+* create a new session view
 */
 Route::get('/login', 'SessionsController@create');
 
 /*
-* create a new user
+* create a new user view
 */
 Route::get('/register', 'RegistrationController@create');
+
+/*
+* store a new user
+*/
+Route::get('/register', 'RegistrationController@store');
