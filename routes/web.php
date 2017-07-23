36062724b682b02d85e15e9f36870e6ac70c03fa
Jeremy Danner <jeremydanner2@gmail.com>
2017-07-23 10:43:59 +0200

    minor routes fixes

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,5 @@
 <?php
-/*
-* create a new session view
-*/
-Route::get('/login', 'SessionsController@create');
 
-/*
-* destroy current session
-*/
-Route::get('/logout', 'SessionsController@destroy');
+Auth::routes();
 
-/*
-* create a new user view
-*/
-Route::get('/register', 'RegistrationController@create');
-
-/*
-* store a new user
-*/
-Route::post('/register', 'RegistrationController@store');
+Route::get('/', 'HomeController@index')->name('home');
