c97a563f0af05656f30ea17b4e200947837b477c
Jeremy Danner <jeremydanner2@gmail.com>
2017-07-24 21:29:40 +0200

    added a new view to the index for fil importer and commented links for excel importer docs

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                @include('layouts.errors')
 
-                <div class="panel-body">
-                    You are logged in!
+                <h2>Importera excel fil</h2>
+
+                <div class="alert alert-info">
+                    information om importer
                 </div>
+
+                <form action="" method="POST">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <input type="file" name="" value="">
+                    </div>
+                    <div class="form-group">
+                        <button class="btn btn-info" type="submit" name="button">Ladda upp excel fil</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+
+
+// https://github.com/Maatwebsite/Laravel-Excel
+// http://www.maatwebsite.nl/laravel-excel/docs/import
