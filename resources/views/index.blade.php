6d82402b4e21a262f8392c1e99682e95bda0bf1b
Jeremy Danner <jeremydanner2@gmail.com>
2017-07-24 21:34:45 +0200

    moved the form to another file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,25 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                @include('layouts.errors')
-
-                <h2>Importera excel fil</h2>
-
-                <div class="alert alert-info">
-                    information om importer
-                </div>
-
-                <form action="" method="POST">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                        <input type="file" name="" value="">
-                    </div>
-                    <div class="form-group">
-                        <button class="btn btn-info" type="submit" name="button">Ladda upp excel fil</button>
-                    </div>
-                </form>
-            </div>
+            {{-- Renders the excel uploader form --}}
+            @include('uploads.form')
         </div>
     </div>
 @endsection
---
+++ resources/views/uploads/form.blade.php
@@ -0,0 +1,19 @@
+<div class="col-md-8 col-md-offset-2">
+    <h2>Importera excel fil</h2>
+
+    <div class="alert alert-info">
+        information om importer
+    </div>
+
+    <form action="" method="POST">
+        {{ csrf_field() }}
+        @include('layouts.errors')
+
+        <div class="form-group">
+            <input type="file" name="" value="">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-info" type="submit" name="button">Ladda upp excel fil</button>
+        </div>
+    </form>
+</div>
