970c7b5e2de58724a0a9769fb2291e3ddf8fa15c
Jeremy Danner <jeremydanner2@gmail.com>
2017-07-26 01:56:48 +0200

    added store route and comments

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
+/*
+* Route for login, register and password resets
+*/
 Auth::routes();
 
+/*
+* Route for home
+*/
 Route::get('/', 'HomeController@index')->name('home');
 
+/*
+* Route to store excel files in the databse.
+*/
+Route::POST('/', 'ExcelController@store')->middleware('auth');
+
+/*
+* Route for the API
+*/
 Route::get('/api/excel', 'ExcelController@index');
 
 // https://github.com/Maatwebsite/Laravel-Excel
