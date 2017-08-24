0797dc41e19385172dce7fb84c55700b09832d93
Jeremy Danner <jeremydanner2@gmail.com>
2017-08-24 14:48:47 +0200

    minor fix to how we save excelfiles. Allso added editorconfig file

--- app/Http/Controllers/ExcelFileController.php
+++ app/Http/Controllers/ExcelFileController.php
@@ -9,78 +9,79 @@ use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
 use Maatwebsite\Excel\Facades\Excel;
 use Illuminate\Support\Facades\Schema;
+use Carbon\Carbon;
 
 class ExcelFileController extends Controller
 {
-  /**
-  * Display a listing of the resource.
-  *
-  * @return JSON
-  */
-  public function index()
-  {
-    $columns = ExcelFile::where('is_deleted', false)->get();
-
-    /*
-    * Will return the query as json to adress /api/excel
+    /**
+    * Display a listing of the resource.
+    *
+    * @return JSON
     */
-    return $columns;
-  }
-
-
-  /**
-  * Store a newly created resource in storage.
-  *
-  * @param  \Illuminate\Http\Request $request
-  * @return \Illuminate\Http\Response
-  */
-  public function store(Request $request)
-  {
-    ExcelFile::truncate();
-
-
-    // TODO: Read file that is uploaded, not local to project!
-    // TODO: Logic for duplicate inputs alt. drop table each time new file is uploaded
-    Excel::filter('chunk')->load(public_path() . '/file.xlsx')->chunk(250, function($results)
+    public function index()
     {
+        $columns = ExcelFile::where('is_deleted', false)->get();
 
-      foreach($results->toArray() as $row)
-      {
+        /*
+        * Will return the query as json to adress /api/excel
+        */
+        return $columns;
+    }
 
-        //  0 => "Material"
-        //  1 => "NOBB nummer"
-        //  2 => "Material                          "
-        //  3 => "Priser 01.04.2017"
-        //  4 => "Enhet"
-        //  5 => null
 
-        ExcelFile::create([
+    /**
+    * Store a newly created resource in storage.
+    *
+    * @param  \Illuminate\Http\Request $request
+    * @return \Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+        ExcelFile::truncate();
 
-          'material'=>$row[0],
-          'designation'=>$row[1],
-          'gross'=>$row[2],
-          // not available in xlsx file, currently hard-coded
-          'currency'=>'SEK',
-          'price_factor'=>$row[4],
-          'unit_price'=>$row[3]
-        ]);
 
-      }
+        // TODO: Read file that is uploaded, not local to project!
+        // TODO: Logic for duplicate inputs alt. drop table each time new file is uploaded
+        // TODO: push all in foreach in array and save outside of foreach.
+        Excel::filter('chunk')->load(public_path() . '/file.xlsx')->chunk(250, function($results)
+        {
 
-    });
+            foreach($results->toArray() as $row)
+            {
+                //  0 => "Material"
+                //  1 => "NOBB nummer"
+                //  2 => "Material                          "
+                //  3 => "Priser 01.04.2017"
+                //  4 => "Enhet"
+                //  5 => null
 
-    return redirect()->home();
+                $excelArray = [
+                    'material'=>$row[0],
+                    'designation'=>$row[1],
+                    'gross'=>$row[2],
+                    // not available in xlsx file, currently hard-coded
+                    'currency'=>'SEK',
+                    'price_factor'=>$row[4],
+                    'unit_price'=>$row[3],
+                    'created_at' => Carbon::now()->format('Y-m-d H:i:s')
+                ];
+                ExcelFile::create($excelArray);
+            }
 
-  }
+        });
 
-  /**
-  * Remove the specified resource from storage.
-  *
-  * @param  int  $id
-  * @return \Illuminate\Http\Response
-  */
-  public function destroy($id)
-  {
-    //
-  }
+        return redirect()->home();
+
+    }
+
+    /**
+    * Remove the specified resource from storage.
+    *
+    * @param  int  $id
+    * @return \Illuminate\Http\Response
+    */
+    public function destroy($id)
+    {
+        //
+    }
 }
